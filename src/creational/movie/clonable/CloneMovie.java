77a1ff8d0ab9cec563d9a8718a54ab0ac85380e4
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-03-12 12:11:13 +0200

    add clone pattern

--- src/creational/movie/clonable/CloneMovie.java
+++ src/creational/movie/clonable/CloneMovie.java
@@ -1,14 +1,17 @@
 package creational.movie.clonable;
 
-public class CloneMovie implements Cloneable{
-    private String title;
-    public CloneMovie(String title) {this.title = title;}
-    public CloneMovie clon() {
-        return new CloneMovie(this.title);
+public class CloneMovie {
+    private final String title;
+
+    public CloneMovie(String title) {
+        this.title = title;
     }
 
-    public void play(){
-        System.out.println("Playing clon movie " + title);
+    public CloneMovie(CloneMovie original) {
+        this.title = original.title;
     }
 
+    public void play() {
+        System.out.println("Playing cloned movie " + title);
+    }
 }
